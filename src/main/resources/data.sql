8fabfa46d17ba107e1c6bfff65cb24ea8a2c04e1
Liubov Evseenkova <griatvablia@yandex.ru>
2026-02-05 11:23:11 +0100

    chore: update README and translate comments to English

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO rating_mpa (name)
 VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 
 INSERT INTO genres (name)
-VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+VALUES ('Comedy'), ('Drama'), ('Animation'), ('Thriller'), ('Documentary'), ('Action');
 
 insert into status (name)
 values ('Accepted'), ('Pending');
